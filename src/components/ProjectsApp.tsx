c060ce7e13b287afcc74200e77afcd5a635d4a22
jmanzanares12 <jimgken2004@gmail.com>
2025-08-18 15:46:27 -0600

    fixed: grammar error in component of ProjectApp

--- src/components/ProjectsApp.tsx
+++ src/components/ProjectsApp.tsx
@@ -5,7 +5,7 @@ const ProjectsApp = () => {
     return (
         <section className='min-h-screen py-6 px-3 md:px-12 lg:px-12 bg-[var(--bg-color)] text-[var(--text-color)]'>
             <h2 className='text-center text-3xl font-bold mb-4'>Mis proyectos</h2>
-            <p className='text-center text-lg text-[var(--text-muted-color)] mb-12'>Laboratios y practicas que he desarrollado con tecnologias modernas y mejorando las buenas prácticas.</p>
+            <p className='text-center text-lg text-[var(--text-muted-color)] mb-12'>Laboratorios y prácticas que he desarrollado con tecnologias modernas y empleando buenas prácticas de desarrollo.</p>
 
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 '>
                 {projectsLinks.map((project, index) => (
