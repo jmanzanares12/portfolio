f1542a4afbcf0b6b7cb25b6bb7be7f914a8a135f
jmanzanares12 <jimgken2004@gmail.com>
2025-12-24 13:23:58 -0600

    Changed: color's globals into portfolio Improved: footer

--- src/components/FooterApp.tsx
+++ src/components/FooterApp.tsx
@@ -3,29 +3,31 @@ import { FaLinkedin } from "react-icons/fa";
 
 const FooterApp = () => {
     return (
-        <footer className="flex flex-row gap-4 items-center justify-center py-8 px-4 bg-[var(--bg-footer-color)] text-center border-t border-[var(--border-color)]">
-            <div className="flex flex-col gap-4 items-center justify-center">
+        <footer className="flex flex-col gap-4 items-center justify-center py-8 px-4 bg-[var(--bg-footer-color)] text-center border-t border-[var(--border-color)]">
+            <div className="flex flex-col gap-4 items-center justify-center text-white">
                 <div>
                     <h2 className="text-base sm:text-lg font-semibold">Jorge Gaitán</h2>
-                    <p className="text-xs sm:text-sm text-[var(--muted-text-color)]">Desarrollador Web Jr</p>
+                    <p className="text-xs sm:text-sm ">Desarrollador Web Jr</p>
                 </div>
 
                 <nav className="flex flex-wrap justify-center gap-4">
-                    <a href="https://github.com/jmanzanares12" target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:bg-black/50 transition-colors duration-300">
+                    <a href="https://github.com/jmanzanares12" target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:text-[#171515] transition-colors duration-300">
                         <AiOutlineGithub size={20} />
                     </a>
-                    <a href="https://www.linkedin.com/in/jorge-manzanares-30b63223a/" target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:bg-cyan-500/50 transition-colors duration-300">
+                    <a href="https://www.linkedin.com/in/jorge-manzanares-30b63223a/" target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:text-[#0A66C2] transition-colors duration-300">
                         <FaLinkedin size={20} />
                     </a>
-                    <a href={`https://wa.me/50576208329?text=${encodeURIComponent('Hola Jorge!')}`} target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:bg-green-500/50 transition-colors duration-300">
+                    <a href={`https://wa.me/50576208329?text=${encodeURIComponent('Hola Jorge!')}`} target="_blank" rel="noreferrer" className="rounded-full px-2 py-2 hover:text-[#25D366] transition-colors duration-300">
                         <AiOutlineWhatsApp size={20} />
                     </a>
                 </nav>
-
-                <p className="text-xs text-[var(--muted-text-color)]">
-                    © {new Date().getFullYear()} Jorge Gaitán. Todos los derechos reservados.
-                </p>
             </div>
+
+            <div className="max-w-7xl border-t border-[var(--border-color)] pt-4 mt-4 w-full">
+                    <p className="text-xs sm:text-sm text-[var(--text-muted-color)]">
+                        © {new Date().getFullYear()} Jorge Gaitán. Todos los derechos reservados.
+                    </p>
+                </div>
         </footer>
     )
 }
