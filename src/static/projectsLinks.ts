83ae79b8a482de4a47d7c2066ddeea8d32d4dbd3
jmanzanares12 <jimgken2004@gmail.com>
2025-12-26 17:37:43 -0600

    fixed: renaming images of projects

--- src/static/projectsLinks.ts
+++ src/static/projectsLinks.ts
@@ -1,51 +1,51 @@
-import Logicsa from '../assets/logicsa.avif';
-import Pokedex from '../assets/pokedex.avif'
-import Horoscope from '../assets/horoscope.avif';
-import RockPaperScissor from '../assets/RockPaperScissor.avif';
-import Pasteleria from '../assets/bakery.avif';
-import RiveraBI from '../assets/RiveraBI.avif';
+import logicsa from '../assets/logicsa.avif';
+import pokedex from '../assets/pokedex.avif'
+import horoscope from '../assets/horoscope.avif';
+import rock_paper_scissor from '../assets/rps.avif';
+import pasteleria from '../assets/bakery.avif';
+import riverabi from '../assets/riverabi.avif';
 
 export const projectsLinks = [
     {
         name: 'Logicsa',
         link: 'https://www.logicsa.net/',
         description: 'Rediseño de pagina web de la empresa Logicsa, dedicada a almacenamiento y gestion de documentos fisicos y digitales.',
-        image: Logicsa,
+        image: logicsa,
         tags: ['html', 'javascript', 'css', 'bootstrap', 'php']
     },
     {
         name: 'Pokedex',
         link: 'https://pokedex-jade-psi.vercel.app/',
         description: 'Poyecto final de curso de introduccion al desarrollo web con React.',
-        image: Pokedex,
+        image: pokedex,
         tags: ['React', 'PokeAPI', 'Tailwind CSS', 'Vercel']
     },
     {
         name: 'Horoscope',
         link: 'https://jmanzanares12.github.io/horoscope-app/',
         description: 'Proyecto de laboratorio para el curso de desarrollo web con React.',
-        image: Horoscope,
+        image: horoscope,
         tags: ['React', 'Tailwind CSS']
     },
     {
         name: 'Rock Paper Scissors',
         link: 'https://jmanzanares12.github.io/rock-paper-scissor/',
         description: 'Proyecto de laboratorio para el curso de desarrollo web con React.',
-        image: RockPaperScissor,
+        image: rock_paper_scissor,
         tags: ['React']
     },
     {
         name: 'Pasteleria Lilliam',
         link: 'https://pasteleria-l.vercel.app/',
         description: 'Proyecto personal para la creación de una pagina web dedicada a la pastelería Lilliam.',
-        image: Pasteleria,
+        image: pasteleria,
         tags: ['React', 'Tailwind CSS', 'JavaScript']
     },
     {
         name: 'RiveraBI',
         link: '',
         description: 'En desarrollo',
-        image: RiveraBI,
+        image: riverabi,
         tags: ['Next.js', 'Tailwind CSS', 'TypeScript', 'PostgreSQL', 'Prisma']
     }
 ]  
